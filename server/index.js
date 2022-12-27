3047d02140a28e36e24b5612e6f68d50d81e9ff0
Yan-ni <anis01000001@gmail.com>
2022-12-27 18:19:19 +0100

    add middlewares

--- server/index.js
+++ server/index.js
@@ -3,15 +3,28 @@ const helmet = require('helmet');
 const cors = require('cors');
 const morgan = require('morgan');
 const path = require('path');
-
+const slowDown = require('express-slow-down');
 const { sequelize } = require('./src/models');
 
 const app = express();
 
-// environement variables
+// load environement variables
 require('dotenv').config();
 
-// middlewares
+// For Nginx
+app.enable('trust proxy');
+
+/* _________________________________ middlewares __________________________________ */
+// IP rate-limiting that slows down responses,
+// rather than blocking the user
+app.use(
+  slowDown({
+    windowMs: 60 * 1000, // 1 minutes
+    delayAfter: 30, // allow 30 requests per 1 minutes, then...
+    delayMs: 500, // begin adding 500ms of delay per request
+  })
+);
+// helps secure the API with various HTTP headers
 app.use(helmet({ crossOriginEmbedderPolicy: false, originAgentCluster: true }));
 app.use(
   helmet.contentSecurityPolicy({
@@ -21,15 +34,19 @@ app.use(
     },
   })
 );
+// enable all cross origin requests
 app.use(cors());
+// HTTP request logger
 app.use(morgan('common'));
+// parses incoming requests with JSON
+app.use(express.json());
 
-// api routes
+/* __________________________________ API routes __________________________________ */
 const routes = require('./src/routes');
 
 app.use('/api', routes);
 
-// view routes
+/* _________________________________ view routes __________________________________ */
 if (process.env.NODE_ENV === 'production') {
   const dist_path = path.resolve(__dirname, '..', 'client', 'dist');
 
@@ -40,7 +57,7 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// start server
+/* _________________________________ start server _________________________________ */
 const PORT = process.env.PORT || 3000;
 
 // sequelize.sync({ force: false }).then(() => {
