2546fef17f12613ece3ce951528e6ec240052144
Yan-ni <anis01000001@gmail.com>
2022-12-27 21:45:59 +0100

    display data & implement search bar

--- client/src/components/Main.jsx
+++ client/src/components/Main.jsx
@@ -4,6 +4,7 @@ import MarkerClusterGroup from 'react-leaflet-cluster';
 import { useEffect, useState } from 'react';
 import axios from 'axios';
 import { v4 as uuidv4 } from 'uuid';
+import debounce from 'lodash.debounce';
 
 const schoolIcon = LeafletIcon({
   iconUrl: new URL('../assets/school.png', import.meta.url).href,
@@ -13,44 +14,94 @@ const schoolIcon = LeafletIcon({
 });
 
 export default function Main() {
-  const [etablissements, setEtablissements] = useState([]);
+  const [schools, setSchools] = useState([]);
+  const [displayedSchools, setDisplayedSchools] = useState(schools);
   useEffect(() => {
     axios
       .get('http://localhost:3000/api/etablissements')
       .then((res) => {
         const { data } = res;
-        setEtablissements(data);
+        setSchools(data);
       })
       .catch((error) => {
         console.error(error);
       });
   }, []);
 
+  useEffect(() => {
+    setDisplayedSchools(schools);
+  }, [schools]);
+
+  const search = (searchPhrase) => {
+    console.log(searchPhrase);
+    if (searchPhrase.trim().length === 0) return setDisplayedSchools(schools);
+
+    return setDisplayedSchools(
+      schools.filter((school) => {
+        if (
+          school.sigle &&
+          school.sigle.toLowerCase().search(searchPhrase.toLowerCase()) !== -1
+        )
+          return true;
+        if (
+          school.uo_lib &&
+          school.uo_lib.toLowerCase().search(searchPhrase.toLowerCase()) !== -1
+        )
+          return true;
+        return false;
+      })
+    );
+  };
+
   return (
     <main>
-      <div className="content">
-        <h1>Etablissements FR</h1>
+      <div className="panel">
+        <div className="panel-control">
+          <form
+            onSubmit={(e) => {
+              e.preventDefault();
+            }}
+          >
+            <input
+              id="search"
+              name="search"
+              type="text"
+              placeholder="Rechercher"
+              onChange={debounce((e) => search(e.target.value), 500)}
+            />
+          </form>
+        </div>
+        <div className="panel-data">
+          <ul>
+            {displayedSchools.map((school) => (
+              <li key={school.etablissement_id_paysage}>
+                {school.sigle} - {school.uo_lib}
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
       <MapContainer
         className="map"
         center={[46.2276, 2.2137]}
         zoom={6}
         minZoom={3}
+        worldCopyJump
       >
         <TileLayer
           attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         />
         <MarkerClusterGroup disableClusteringAtZoom={14} key={uuidv4()}>
-          {etablissements.map((etablissement) => {
-            if (etablissement.coordonnees !== undefined)
+          {displayedSchools.map((school) => {
+            if (school.coordonnees !== undefined)
               return (
                 <Marker
-                  position={etablissement.coordonnees}
+                  position={school.coordonnees}
                   icon={schoolIcon}
-                  key={etablissement.etablissement_id_paysage}
+                  key={school.etablissement_id_paysage}
                 >
-                  <Popup>{etablissement.uo_lib}</Popup>
+                  <Popup>{school.uo_lib}</Popup>
                 </Marker>
               );
             return null;
