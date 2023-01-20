bef7788eab22b2b6d716876fdd2fb5693d417c6f
NBAdrM <adrien.maestri@gmail.com>
2023-01-20 16:46:47 +0100

    add select in FilterModal

--- client/src/components/FilterModal.jsx
+++ client/src/components/FilterModal.jsx
@@ -1,11 +1,36 @@
-export default function FilterModal({ setIsOpen }) {
+import React, { useState } from 'react';
+import { observer } from 'mobx-react';
+
+function FilterModal({ setIsOpen, filterStore }) {
+  const secteurs = ['public', 'privé'];
+  const type = ['université','école','autre établisement']
+
   return (
     <div className="modal-overlay">
       <form className="modal-content" onSubmit={(e) => e.preventDefault()}>
         <button type="button" onClick={() => setIsOpen(false)}>
           X
         </button>
-        <div className="inputs" />
+        <div className="inputs">
+          <pre>Secteur : {filterStore.secteur}</pre>
+          <select value={filterStore.secteur} onChange={e => filterStore.setSecteur(e.target.value)}>
+            <option value="">--secteur--</option>
+            {secteurs.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+          <pre>Type : {filterStore.type}</pre>
+          <select value={filterStore.type} onChange={e => filterStore.setType(e.target.value)}>
+            <option value="">--type--</option>
+            {type.map((option, index) => (
+              <option key={index} value={option}>
+                {option}
+              </option>
+            ))}
+          </select>
+        </div>
         <div className="button-group">
           <button type="button" onClick={() => setIsOpen(false)}>
             Annuler
@@ -16,3 +41,6 @@ export default function FilterModal({ setIsOpen }) {
     </div>
   );
 }
+
+
+export default observer(FilterModal);
--- client/src/stores/filterStore.js
+++ client/src/stores/filterStore.js
@@ -5,13 +5,19 @@ class FilterStore {
     makeObservable(this, {
       secteur: observable,
       setSecteur: action,
+      type: observable,
+      setType: action,
     });
   }
 
   secteur = null;
+  type = null;
 
   setSecteur(state) {
     this.secteur = state;
+  }
+  setType(state) {
+    this.type = state;
   }
 }
 
